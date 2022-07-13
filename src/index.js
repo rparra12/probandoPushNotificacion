33320adbca686fdeb9847a66bfd315a1f39c70fb
royparra12 <geoyachaq.comunicaciones@gmail.com>
2022-07-13 10:38:03 -0500

    asd

--- src/index.js
+++ src/index.js
@@ -21,6 +21,8 @@ app.use(require('./routes/index'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
 console.log("asd");
 
